189aa2864480574f412be2c41c62f63fab747e7c
Anand Kashyap <anandkashyap60@gmail.com>
2021-05-23 14:27:09 +0530

    typography update in cards

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Star from './Star'
 
 const Restaurant = ({ name, delivery_time, food_types, price_for_two, ratings }) => {
   return (
@@ -7,12 +8,15 @@ const Restaurant = ({ name, delivery_time, food_types, price_for_two, ratings })
       <p className='res-name'>{name}</p>
       {food_types && <p className='res-text'>{food_types.join(', ')}</p>}
       <div className='res-text res-stats'>
-        <div className='res-rating'>
-          {ratings || '-'}
+        <div className={`res-rating ${!ratings ? 'no-rating' : ''}`}>
+          <Star isWhite={!!ratings} />
+          <span>
+            {ratings || '-'}
+          </span>
         </div>
-        <div>.</div>
+        <div className='dot-separator'>.</div>
         <p className='res-delivery-time'>{delivery_time}</p>
-        <div>.</div>
+        <div className='dot-separator'>.</div>
         <p className='res-price'>₹{price_for_two} for two</p>
       </div>
       <div className='quick-view'>
